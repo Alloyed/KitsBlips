020dfb94c8efb449d0e2b608004d245894b5741f
Kyle McLamb <kjmclamb@gmail.com>
2024-11-07 20:06:30 -0500

    add formula 1 + 3

--- dsp/src/dsfOscillator.cpp
+++ dsp/src/dsfOscillator.cpp
@@ -7,16 +7,28 @@
 namespace
 {
     /**
+     * sidebands up, band-limited
+     */
+    float moorerFormula1(float theta, float beta, float N, float a)
+    {
+        float maxAmplitude = (1.0f - powf(a, N)) / (1.0f - a);
+
+        float bandlimit = powf(a, N + 1) * (sinf(theta + (N + 1) * beta) - (a * sinf(theta + (N * beta))));
+        float num = sinf(theta) - (a * sinf(theta - beta)) - bandlimit;
+        float denom = 1.0f + (a * a) - (2.0f * a * cosf(beta));
+
+        return num / (denom * maxAmplitude);
+    }
+
+    /**
      * sidebands up, not band-limited
      */
-    inline float moorerFormula2(float theta, float beta, float a)
+    float moorerFormula2(float theta, float beta, float N, float a)
     {
         // this is the infinite form, so N is technically also infinite. I just picked a big number :)
-        float N = 8;
-        // replace powf(a, N)
-        float aPowN = a * a * a * a;
+        float N = 16;
 
-        float maxAmplitude = (1.0f - aPowN) / (1.0f - a);
+        float maxAmplitude = (1.0f - powf(a, N)) / (1.0f - a);
 
         float num = sinf(theta) - (a * sinf(theta - beta));
         float denom = 1.0f + (a * a) - (2.0f * a * cosf(beta));
@@ -25,12 +37,27 @@ namespace
     }
 
     /**
+     * sidebands up+down, band-limited
+     */
+    float moorerFormula3(float theta, float beta, float N, float a)
+    {
+        float maxAmplitude = (1.0f - powf(a, N)) / (1.0f - a);
+
+        float a2 = a * a;
+
+        float num = sinf(theta) * (1.0f - a2 - (2.0f * powf(a, N + 1) * (cosf((N + 1.0f) * beta) - a * cosf(N * beta))));
+        float denom = 1 + (a2) - (2 * a * cosf(beta));
+
+        return num / (denom * maxAmplitude);
+    }
+
+    /**
      * sidebands up+down, not band-limited
      */
-    float moorerFormula4(float theta, float beta, float a)
+    float moorerFormula4(float theta, float beta, float N, float a)
     {
         // this is the infinite form, so N is technically also infinite. I just picked a big number :)
-        float N = 8;
+        float N = 16;
 
         float maxAmplitude = (1.0f - powf(a, N)) / (1.0f - a);
 
@@ -44,14 +71,10 @@ namespace
 float DsfOscillator::Process()
 {
     mPhaseCarrier += mFreqCarrier * mSecondsPerSample;
-    if (mPhaseCarrier > 1.0f)
-    {
-        mPhaseCarrier -= 1.0f;
-    }
+    mPhaseCarrier = fmodf(mPhaseCarrier, 1.0f);
+
     mPhaseModulator += mFreqModulator * mSecondsPerSample;
-    if (mPhaseModulator > 1.0f)
-    {
-        mPhaseModulator -= 1.0f;
-    }
-    return moorerFormula2(mPhaseCarrier * cTwoPi, mPhaseModulator * cTwoPi, mFalloff);
+    mPhaseModulator = fmodf(mPhaseModulator, 1.0f);
+
+    return moorerFormula2(mPhaseCarrier * cTwoPi, mPhaseModulator * cTwoPi, 8, mFalloff);
 }
