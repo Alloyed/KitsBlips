e8bb84f30a4691848577783462a64fd63f4e5a48
Kyle McLamb <kjmclamb@gmail.com>
2024-11-23 12:37:29 -0500

    adsr iteration/fixing

--- daisy/adsr/adsr.cpp
+++ daisy/adsr/adsr.cpp
@@ -3,12 +3,7 @@
 
 #include <kitdsp/util.h>
 
-const float cEpsilon = 0.01f;
-bool nearf(float a, float b)
-{
-    return fabsf(a - b) <= cEpsilon;
-}
-
+const float cEpsilon = 0.005f;
 float lerpPowf(float a, float b, float curve, float t)
 {
     return lerpf(a, b, clampf(powf(t, curve), 0.0f, 1.0f));
@@ -40,44 +35,41 @@ public:
         switch (state)
         {
         case State::Attack:
-            if (nearf(mAttackTime, value))
-            {
-                return;
-            }
+        {
+
             mAttackTime = value;
             mAttackApproach = calculateApproach(mAttackTime, 0.0f, 1.0f);
-            break;
+        }
+        break;
         case State::Decay:
-            if (nearf(mDecayTime, value))
-            {
-                return;
-            }
+        {
+
             mDecayTime = value;
             mDecayApproach = calculateApproach(mDecayTime, 1.0f, mSustainLevel);
-            break;
+        }
+        break;
         case State::Sustain:
-            if (nearf(mSustainLevel, value))
-            {
-                return;
-            }
+        {
             mSustainLevel = value;
             mDecayApproach = calculateApproach(mDecayTime, 1.0f, mSustainLevel);
             mReleaseApproach = calculateApproach(mReleaseTime, mSustainLevel, 0.0f);
-            break;
+        }
+        break;
         case State::Release:
-            if (nearf(mReleaseTime, value))
-            {
-                return;
-            }
+        {
             mReleaseTime = value;
             mReleaseApproach = calculateApproach(mReleaseTime, mSustainLevel, 0.0f);
-            break;
+        }
+        break;
+        case State::Idle:
+            return;
         }
     }
 
     float Process(bool gate, bool retrigger)
     {
-        // calculate current state
+        // calculate state transistions
+        retrigger = gate || retrigger;
         if (gate || retrigger)
         {
             if (retrigger && !mLastRetrigger)
@@ -89,12 +81,12 @@ public:
             {
                 mState = State::Attack;
             }
-            else if (mState == State::Attack && nearf(mLastLevel, 1.0f))
+            else if (mState == State::Attack && mLastLevel >= 1.0f - cEpsilon)
             {
                 mLastLevel = 1.0f;
                 mState = State::Decay;
             }
-            else if (mState == State::Decay && nearf(mLastLevel, mSustainLevel))
+            else if (mState == State::Decay && mLastLevel <= mSustainLevel + cEpsilon)
             {
                 mLastLevel = mSustainLevel;
                 mState = State::Sustain;
@@ -106,7 +98,7 @@ public:
             {
                 mState = State::Release;
             }
-            else if (mState == State::Release && nearf(mLastLevel, 0.0f))
+            else if (mState == State::Release && mLastLevel <= 0.0f + cEpsilon)
             {
                 mLastLevel = 0.0f;
                 mState = State::Idle;
@@ -116,11 +108,7 @@ public:
 
         // update & return value based on state
         float targetApproach, targetLevel;
-        if (mState == State::Idle)
-        {
-            return mLastLevel;
-        }
-        else if (mState == State::Attack)
+        if (mState == State::Attack)
         {
             targetApproach = mAttackApproach;
             targetLevel = 1.0f;
@@ -130,18 +118,25 @@ public:
             targetApproach = mDecayApproach;
             targetLevel = mSustainLevel;
         }
-        if (mState == State::Sustain)
+        else if (mState == State::Sustain)
         {
             // this could be smoothed too to account for real time changes in sustain level, if necessary
-            mLastLevel = mSustainLevel;
-            return mLastLevel;
+            targetApproach = mDecayApproach;
+            targetLevel = mSustainLevel;
+            // return mLastLevel;
         }
         else if (mState == State::Release)
         {
             targetApproach = mReleaseApproach;
             targetLevel = 0.0f;
         }
+        else if (mState == State::Idle)
+        {
+            // return mLastLevel;
+            return 0.0f;
+        }
 
+        // mLastLevel = targetLevel;
         mLastLevel = lerpf(mLastLevel, targetLevel, targetApproach);
         return mLastLevel;
     }
@@ -156,23 +151,23 @@ private:
     {
         // https://www.youtube.com/watch?v=LSNQuFEDOyQ
         // the target precision should be whatever gets us within cEpsilon of the target, in absolute terms
-        float precision = cEpsilon / (nextLevel - lastLevel);
+        float precision = cEpsilon * fabsf(nextLevel - lastLevel);
         return 1.0f - blockNanf(powf(precision, mSampleTimeMs / targetTimeMs));
     }
 
     float mSampleTimeMs;
 
-    State mState;
-    float mLastLevel;
-    bool mLastRetrigger;
+    State mState{State::Idle};
+    float mLastLevel{0.0f};
+    bool mLastRetrigger{false};
 
-    float mAttackTime;
-    float mAttackApproach;
-    float mDecayTime;
-    float mDecayApproach;
-    float mSustainLevel;
-    float mReleaseApproach;
-    float mReleaseTime;
+    float mAttackTime{0.0f};
+    float mAttackApproach{1.0f};
+    float mDecayTime{0.0f};
+    float mDecayApproach{1.0f};
+    float mSustainLevel{1.0f};
+    float mReleaseApproach{1.0f};
+    float mReleaseTime{0.0f};
 };
 
 /**
@@ -196,7 +191,6 @@ float jackValue(int32_t cvEnum)
     return clampf(hw.controls[cvEnum].Value(), -1.0f, 1.0f);
 }
 
-
 void AudioCallback(AudioHandle::InputBuffer in,
                    AudioHandle::OutputBuffer out,
                    size_t size)
@@ -205,12 +199,12 @@ void AudioCallback(AudioHandle::InputBuffer in,
     button.Debounce();
     toggle.Debounce();
 
-    bool gate = hw.gate_in_1.State();
+    bool gate = hw.gate_in_1.State() || button.Pressed();
     bool retrig = hw.gate_in_2.Trig();
 
     float a = lerpPowf(3.0f, 10000.0f, 2.0f, knobValue(CV_1) + jackValue(CV_5));
     float d = lerpPowf(3.0f, 10000.0f, 2.0f, knobValue(CV_2) + jackValue(CV_6));
-    float s = lerpf(0.0f, 1.0f, clampf(0.0f, 1.0f, knobValue(CV_3) + jackValue(CV_7)));
+    float s = lerpf(0.0f, 1.0f, clampf(knobValue(CV_3) + jackValue(CV_7), 0.0f, 1.0f));
     float r = lerpPowf(3.0f, 10000.0f, 2.0f, knobValue(CV_4) + jackValue(CV_8));
 
     adsr.Set(ApproachAdsr::State::Attack, a);
@@ -222,11 +216,11 @@ void AudioCallback(AudioHandle::InputBuffer in,
     {
         float level = adsr.Process(gate, retrig);
         OUT_L[i] = -level;
-        OUT_R[i] = level-1.0f;
+        OUT_R[i] = level - 1.0f;
     }
 
     // visualize
-    hw.WriteCvOut(CV_OUT_2, lerpf(1.6f, 5.0f, OUT_L[size - 1]));
+    hw.WriteCvOut(CV_OUT_2, lerpf(0.0f, 5.0f, -OUT_L[size - 1]));
 
     // stage gates!
     // A+D and S+R combined for lack of open jacks
@@ -240,10 +234,11 @@ int main(void)
     hw.Init();
     // low sample rate low block size for cv workload
     hw.SetAudioBlockSize(1);
-    hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_8KHZ);
+    hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_32KHZ);
 
     button.Init(DaisyPatchSM::B7, hw.AudioCallbackRate());
     toggle.Init(DaisyPatchSM::B8, hw.AudioCallbackRate());
+    adsr.Init(hw.AudioSampleRate());
     hw.StartAudio(AudioCallback);
 
     for (;;)
