668e89e7291456bbb7708a14cc4cf139398cd140
Kyle McLamb <kjmclamb@gmail.com>
2025-05-21 07:00:48 -0400

    minor CI speedup by disabling SDL subsystems

--- content.cmake
+++ content.cmake
@@ -52,13 +52,28 @@ FetchContent_Declare(
     GIT_TAG main
 )
 
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 set(SDL_SHARED OFF)
 set(SDL_STATIC ON)
 set(SDL_TESTS OFF)
-set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+# SDL subsystems
+set(SDL_AUDIO    OFF)
+set(SDL_VIDEO    ON)
+set(SDL_GPU      OFF)
+set(SDL_RENDER   OFF)
+set(SDL_CAMERA   OFF)
+set(SDL_JOYSTICK OFF)
+set(SDL_HAPTIC   OFF)
+set(SDL_HIDAPI   OFF)
+set(SDL_POWER    OFF)
+set(SDL_SENSOR   OFF)
+set(SDL_DIALOG   OFF)
+# https://github.com/libsdl-org/SDL/issues/6454
+if(APPLE)
+    enable_language(OBJC)
+endif()
 FetchContent_Declare(
     SDL3
     GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
     GIT_TAG release-3.2.14
-    OVERRIDE_FIND_PACKAGE
 )
