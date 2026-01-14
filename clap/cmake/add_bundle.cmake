43f0463c12630282d3f1a0bfa7dc98e9943b3e58
Kyle <kyle@kmac.local>
2026-01-14 09:09:50 -0500

    mac fixes

--- clap/cmake/add_bundle.cmake
+++ clap/cmake/add_bundle.cmake
@@ -8,12 +8,11 @@ function(add_bundle TARGET_NAME OUTPUT_FILE ASSETS_DIR)
 
     cmake_path(GET ASSETS_DIR PARENT_PATH WORKING_DIR)
     cmake_path(GET OUTPUT_FILE PARENT_PATH OUTPUT_DIR)
-    set(ZIP_FILE "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}_assets.zip")
+    set(ZIP_FILE "${CMAKE_CURRENT_BINARY_DIR}/tmp/${TARGET_NAME}_assets.zip")
     set(TARGET_FILE "$<TARGET_FILE:${TARGET_NAME}>")
 
     add_custom_command(
-        TARGET ${TARGET_NAME}
-        POST_BUILD
+        TARGET ${TARGET_NAME} POST_BUILD
         WORKING_DIRECTORY "${WORKING_DIR}" # use so final zip looks like project dir
         COMMAND ${CMAKE_COMMAND} -E tar c "${ZIP_FILE}" --format=zip "${ASSETS_DIR}"
         COMMAND ${CMAKE_COMMAND} -E make_directory "${OUTPUT_DIR}"
