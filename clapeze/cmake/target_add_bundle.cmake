1f00951c45ac343ff3addabe3f1b98a567c29aab
Alloyed <kjmclamb@gmail.com>
2026-01-16 21:14:43 -0500

    ough

--- clapeze/cmake/target_add_bundle.cmake
+++ clapeze/cmake/target_add_bundle.cmake
@@ -1,5 +1,5 @@
 # Use to add a bundled output to your library/executable target
-function(target_add_bundle TARGET_NAME ASSETS_DIR)
+function(target_add_bundle TARGET_NAME ASSETS_DIR OUT_FILE)
     if (NOT TARGET ${TARGET_NAME})
         message(FATAL_ERROR "Target '${TARGET_NAME}' does not exist")
     endif()
@@ -7,21 +7,31 @@ function(target_add_bundle TARGET_NAME ASSETS_DIR)
         message(FATAL_ERROR "Directory '${ASSETS_DIR}' does not exist")
     endif()
 
+    # make zip
     set(ZIP_DIR "${CMAKE_CURRENT_BINARY_DIR}/tmp")
     set(ZIP_FILE "${ZIP_DIR}/${TARGET_NAME}_assets.zip")
-    set(TARGET_FILE "$<TARGET_FILE:${TARGET_NAME}>")
     cmake_path(GET ASSETS_DIR PARENT_PATH WORKING_DIR)
-    
     file(GLOB_RECURSE GLOBBED_ASSETS CONFIGURE_DEPENDS "${ASSETS_DIR}/*")
-    add_custom_target(${TARGET_NAME}_assets SOURCES ${GLOBBED_ASSETS})
-    add_dependencies(${TARGET_NAME} ${TARGET_NAME}_assets)
     add_custom_command(
-        TARGET ${TARGET_NAME} POST_BUILD
+        OUTPUT ${ZIP_FILE}
+        DEPENDS ${GLOBBED_ASSETS}
         WORKING_DIRECTORY "${WORKING_DIR}" # use so final zip looks like project dir
         COMMAND ${CMAKE_COMMAND} -E make_directory "${ZIP_DIR}"
         COMMAND ${CMAKE_COMMAND} -E tar c "${ZIP_FILE}" --format=zip "${ASSETS_DIR}"
-        COMMAND ${CMAKE_COMMAND} -E cat "${TARGET_FILE}" "${ZIP_FILE}" > "${TARGET_FILE}"
-        COMMENT "adding asset bundle to: ${TARGET_FILE}"
+        COMMENT "creating zip file: ${ZIP_FILE}"
         VERBATIM
     )
+
+    # make bundle
+    set(TARGET_FILE "$<TARGET_FILE:${TARGET_NAME}>")
+    cmake_path(GET OUT_FILE PARENT_PATH OUT_DIR)
+    add_custom_command(
+        OUTPUT ${OUT_FILE}
+        DEPENDS ${TARGET_NAME} ${ZIP_FILE}
+        COMMAND ${CMAKE_COMMAND} -E make_directory "${OUT_DIR}"
+        COMMAND ${CMAKE_COMMAND} -E cat "${TARGET_FILE}" "${ZIP_FILE}" > "${OUT_FILE}"
+        COMMENT "adding asset bundle to: ${OUT_FILE}"
+        VERBATIM
+    )
+    add_custom_target(${TARGET_NAME}_bundled ALL SOURCES ${OUT_FILE})
 endfunction()
