c44eb3b3c0a976b002e4d0772bb2735bee2e874b
Alloyed <kjmclamb@gmail.com>
2026-05-07 07:53:28 -0400

    transfer functions (for later)

--- kitdsp/include/kitdsp/filters/biquad.h
+++ kitdsp/include/kitdsp/filters/biquad.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cmath>
+#include <complex>
 #include "kitdsp/math/util.h"
 
 namespace kitdsp {
@@ -66,6 +67,9 @@ class BiquadFilter {
         CalculateCoefficients<MODE>();
     }
 
+    template <BiquadFilterMode MODE>
+    inline std::complex<float> TransferFunction(std::complex<float> s);
+
     inline float Process(float in) {
         // clang-format off
         float out = (b0 * in)
@@ -108,6 +112,11 @@ class BiquadFilter {
 };
 
 template <>
+inline std::complex<float> BiquadFilter::TransferFunction<BiquadFilterMode::LowPass>(std::complex<float> s) {
+    return 1.0f / ((s * s) + s / Q + 1.0f);
+}
+
+template <>
 inline void BiquadFilter::CalculateCoefficients<BiquadFilterMode::LowPass>() {
     // H(s) = 1 / (s^2 + s/Q + 1)
     float alpha = sin_w0 / (2.0f * Q);
@@ -118,6 +127,12 @@ inline void BiquadFilter::CalculateCoefficients<BiquadFilterMode::LowPass>() {
     b2 = ((1.0f - cos_w0) / 2.0f) / a0;
     a1 = (-2.0f * cos_w0) / a0;
     a2 = (1.0f - alpha) / a0;
+}
+
+template <>
+inline std::complex<float> BiquadFilter::TransferFunction<BiquadFilterMode::Highpass>(std::complex<float> s) {
+    // H(s) = s^2 / (s^2 + s/Q + 1)
+    return (s * s) / ((s * s) + (s / Q) + 1.0f);
 }
 
 template <>
@@ -132,6 +147,12 @@ inline void BiquadFilter::CalculateCoefficients<BiquadFilterMode::Highpass>() {
     a1 = (-2.0f * cos_w0) / a0;
     a2 = (1.0f - alpha) / a0;
 }
+
+template <>
+inline std::complex<float> BiquadFilter::TransferFunction<BiquadFilterMode::BandPass>(std::complex<float> s) {
+    // H(s) = s / (s^2 + s/Q + 1)  (constant skirt gain, peak gain = Q)
+    return s / ((s * s) + (s / Q) + 1.0f);
+}
 template <>
 inline void BiquadFilter::CalculateCoefficients<BiquadFilterMode::BandPass>() {
     // H(s) = s / (s^2 + s/Q + 1)  (constant skirt gain, peak gain = Q)
@@ -143,6 +164,12 @@ inline void BiquadFilter::CalculateCoefficients<BiquadFilterMode::BandPass>() {
     b2 = (-sin_w0 / 2.0f) / a0;
     a1 = (-2.0f * cos_w0) / a0;
     a2 = (1.0f - alpha) / a0;
+}
+
+template <>
+inline std::complex<float> BiquadFilter::TransferFunction<BiquadFilterMode::Notch>(std::complex<float> s) {
+    // H(s) = (s^2 + 1) / (s^2 + s/Q + 1)
+    return (s * s + 1.0f) / ((s * s) + (s / Q) + 1.0f);
 }
 template <>
 inline void BiquadFilter::CalculateCoefficients<BiquadFilterMode::Notch>() {
@@ -156,6 +183,12 @@ inline void BiquadFilter::CalculateCoefficients<BiquadFilterMode::Notch>() {
     a1 = b1;
     a2 = (1.0f - alpha) / a0;
 }
+
+template <>
+inline std::complex<float> BiquadFilter::TransferFunction<BiquadFilterMode::AllPass>(std::complex<float> s) {
+    // H(s) = (s^2 - s/Q + 1) / (s^2 + s/Q + 1)
+    return ((s * s) - (s / Q) + 1.0f) / ((s * s) + (s / Q) + 1.0f);
+}
 template <>
 inline void BiquadFilter::CalculateCoefficients<BiquadFilterMode::AllPass>() {
     // H(s) = (s^2 - s/Q + 1) / (s^2 + s/Q + 1)
@@ -167,6 +200,12 @@ inline void BiquadFilter::CalculateCoefficients<BiquadFilterMode::AllPass>() {
     b2 = 1.0f;  // (1+alpha) / a0
     a1 = b1;
     a2 = b0;
+}
+
+template <>
+inline std::complex<float> BiquadFilter::TransferFunction<BiquadFilterMode::LowShelf>(std::complex<float> s) {
+    // H(s) = A * (s^2 + (sqrt(A)/Q)*s + A)/(A*s^2 + (sqrt(A)/Q)*s + 1)
+    return A * (((s * s) + (std::sqrt(A) / Q) * s + A) / (A * s * s + (std::sqrt(A) / Q) * s + 1.0f));
 }
 template <>
 inline void BiquadFilter::CalculateCoefficients<BiquadFilterMode::LowShelf>() {
@@ -180,6 +219,12 @@ inline void BiquadFilter::CalculateCoefficients<BiquadFilterMode::LowShelf>() {
     b2 = A * (A_p_1 - A_m_1 * cos_w0 - twoSqrtAAlpha) / a0;
     a1 = -2.0f * (A_m_1 + A_p_1 * cos_w0) / a0;
     a2 = (A_p_1 + A_m_1 * cos_w0 - twoSqrtAAlpha) / a0;
+}
+
+template <>
+inline std::complex<float> BiquadFilter::TransferFunction<BiquadFilterMode::HighShelf>(std::complex<float> s) {
+    // H(s) = A * (A*s^2 + (sqrt(A)/Q)*s + 1)/(s^2 + (sqrt(A)/Q)*s + A)
+    return A * (A * s * s + (std::sqrt(A) / Q) * s + 1.0f) / (s * s + (std::sqrt(A) / Q) * s + A);
 }
 template <>
 inline void BiquadFilter::CalculateCoefficients<BiquadFilterMode::HighShelf>() {
